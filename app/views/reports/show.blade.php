e44c6b73f580664e195d2d7c9c7c71c83545e815
SalomonTapia <salomontapiaaguilar@hotmail.com>
2014-11-27 07:27:57 -0600

    last changes

--- app/views/reports/show.blade.php
+++ app/views/reports/show.blade.php
@@ -12,5 +12,36 @@
 <strong>Descripcion:</strong><br>
 	{{$report->desc_reporte}}
 </p>
+<div align="left" style="border: 3px solid green; margin-botton:1px;">
+-		@foreach($report->comments as $coment)
+-		<div>
+-			<strong><span>Por: {{$coment->user->nombre}} el {{$coment->created_at}}</span></strong>
+-			<p></p>
+-			<p align="left">
+-			<span class="list-inline">
+-				{{Form::open(array('class'=>'form-horizontal pull-right','method'=>'DELETE','route'=>array('reports.comentrs.destroy',$report->id,$coment->id)))}}
+-				{{Form::submit('Eliminar',array('class'=>'btn btn-sm btn-danger'))}}
+-				{{Form::close()}}
+-				</span>
+-			<p>	{{$coment->comentario}} </p>
+-			</p>
+-			
+-		</div>
+-		@endforeach
+-	</div>
+-<div align="center">
+-		<h3>
+-			<strong>Agregar nuevo comentario</strong>
+-		</h3>
+-	</div>
+-	<div align="center">
+-		@include('comentrs/partials/_coment')
+-	</div>
+-<p align="center">
+-	{{link_to_route('reports.index','Volver a mis noticias')}}
+-</p>
+-<p>
+-	{{link_to_route('reports.edit','Editar noticia', $report->id)}}
+-</p>
 
 @stop
